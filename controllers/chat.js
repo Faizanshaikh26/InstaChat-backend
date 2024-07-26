436ee3da1263852555f4824be8e8dc1c74e5a6de
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-07-26 16:38:37 +0530

    done

--- controllers/chat.js
+++ controllers/chat.js
@@ -50,8 +50,8 @@ const getMyChats = TryCatch(async (req, res, next) => {
       _id,
       groupChat,
       avatar: groupChat
-        ? members.slice(0, 3).map(({ avatar }) => avatar.url)
-        : [otherMember.avatar.url],
+        ? members?.slice(0, 3).map(({ avatar }) => avatar?.url)
+        : [otherMember?.avatar?.url],
       name: groupChat ? name : otherMember.name,
       members: members.reduce((prev, curr) => {
         if (curr._id.toString() !== req.user.toString()) {
