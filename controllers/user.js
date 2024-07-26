6588be27b7acda57ae7a8ab6951e4ca3adff3df3
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-07-26 12:41:06 +0530

    almost-done

--- controllers/user.js
+++ controllers/user.js
@@ -153,7 +153,7 @@ const resetPassword = async (req, res) => {
 };
 
 const getMyProfile = TryCatch(async (req, res, next) => {
-  const user = await User.findById(req.user);
+  const user = await User.findById(req.user).lean();
 
   if (!user) return next(new ErrorHandler("User not found", 404));
 
